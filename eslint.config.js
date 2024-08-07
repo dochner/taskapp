debc8af5027fd882c98db789f18e3d871f33969a
Douglas Ochner <douglas.ochner@gmail.com>
2024-08-07 21:59:55 +0100

    chore: update eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -4,239 +4,238 @@ import nuxt from './.nuxt/eslint.config.mjs'
 export default nuxt(
   antfu(
     {
-      unocss: true,
       formatters: true,
     },
     {
-      "rules": {
-        "no-unused-vars": [
-          "error",
+      rules: {
+        'no-unused-vars': [
+          'error',
           {
-            "ignoreRestSiblings": true
-          }
+            ignoreRestSiblings: true,
+          },
         ],
-        "vue/max-attributes-per-line": [
-          "error",
+        'vue/max-attributes-per-line': [
+          'error',
           {
-            "singleline": {
-              "max": 1
+            singleline: {
+              max: 1,
             },
-            "multiline": {
-              "max": 1
-            }
-          }
+            multiline: {
+              max: 1,
+            },
+          },
         ],
-        "vue/attributes-order": [
-          "error",
+        'vue/attributes-order': [
+          'error',
           {
-            "order": [
-              "DEFINITION",
-              "LIST_RENDERING",
-              "CONDITIONALS",
-              "RENDER_MODIFIERS",
-              "GLOBAL",
+            order: [
+              'DEFINITION',
+              'LIST_RENDERING',
+              'CONDITIONALS',
+              'RENDER_MODIFIERS',
+              'GLOBAL',
               [
-                "UNIQUE",
-                "SLOT"
+                'UNIQUE',
+                'SLOT',
               ],
-              "TWO_WAY_BINDING",
-              "OTHER_DIRECTIVES",
-              "OTHER_ATTR",
-              "EVENTS",
-              "CONTENT"
+              'TWO_WAY_BINDING',
+              'OTHER_DIRECTIVES',
+              'OTHER_ATTR',
+              'EVENTS',
+              'CONTENT',
             ],
-            "alphabetical": true
-          }
+            alphabetical: true,
+          },
         ],
-        "vue/no-v-html": "off",
-        "vue/require-prop-types": "off",
-        "vue/require-default-prop": "off",
-        "vue/multi-word-component-names": "off",
-        "vue/prefer-import-from-vue": "off",
-        "vue/no-v-text-v-html-on-component": "off",
-        "vue/no-setup-props-destructure": "off",
-        "vue/component-tags-order": [
-          "error",
+        'vue/no-v-html': 'off',
+        'vue/require-prop-types': 'off',
+        'vue/require-default-prop': 'off',
+        'vue/multi-word-component-names': 'off',
+        'vue/prefer-import-from-vue': 'off',
+        'vue/no-v-text-v-html-on-component': 'off',
+        'vue/no-setup-props-destructure': 'off',
+        'vue/component-tags-order': [
+          'error',
           {
-            "order": [
-              "script",
-              "template",
-              "style"
-            ]
-          }
+            order: [
+              'script',
+              'template',
+              'style',
+            ],
+          },
         ],
-        "vue/block-tag-newline": [
-          "error",
+        'vue/block-tag-newline': [
+          'error',
           {
-            "singleline": "always",
-            "multiline": "always"
-          }
+            singleline: 'always',
+            multiline: 'always',
+          },
         ],
-        "vue/component-name-in-template-casing": [
-          "error",
-          "PascalCase"
+        'vue/component-name-in-template-casing': [
+          'error',
+          'PascalCase',
         ],
-        "vue/component-options-name-casing": [
-          "error",
-          "PascalCase"
+        'vue/component-options-name-casing': [
+          'error',
+          'PascalCase',
         ],
-        "vue/define-macros-order": [
-          "error",
+        'vue/define-macros-order': [
+          'error',
           {
-            "order": [
-              "defineProps",
-              "defineEmits"
-            ]
-          }
+            order: [
+              'defineProps',
+              'defineEmits',
+            ],
+          },
         ],
-        "vue/html-comment-content-spacing": [
-          "error",
-          "always",
+        'vue/html-comment-content-spacing': [
+          'error',
+          'always',
           {
-            "exceptions": [
-              "-"
-            ]
-          }
+            exceptions: [
+              '-',
+            ],
+          },
         ],
-        "vue/no-restricted-v-bind": [
-          "error",
-          "/^v-/"
+        'vue/no-restricted-v-bind': [
+          'error',
+          '/^v-/',
         ],
-        "vue/no-useless-v-bind": "error",
-        "vue/no-unused-refs": "error",
-        "vue/padding-line-between-blocks": [
-          "error",
-          "always"
+        'vue/no-useless-v-bind': 'error',
+        'vue/no-unused-refs': 'error',
+        'vue/padding-line-between-blocks': [
+          'error',
+          'always',
         ],
-        "vue/prefer-separate-static-class": "error",
-        "vue/array-bracket-spacing": [
-          "error",
-          "never"
+        'vue/prefer-separate-static-class': 'error',
+        'vue/array-bracket-spacing': [
+          'error',
+          'never',
         ],
-        "vue/arrow-spacing": [
-          "error",
+        'vue/arrow-spacing': [
+          'error',
           {
-            "before": true,
-            "after": true
-          }
+            before: true,
+            after: true,
+          },
         ],
-        "vue/block-spacing": [
-          "error",
-          "always"
+        'vue/block-spacing': [
+          'error',
+          'always',
         ],
-        "vue/brace-style": [
-          "error",
-          "stroustrup",
+        'vue/brace-style': [
+          'error',
+          'stroustrup',
           {
-            "allowSingleLine": true
-          }
+            allowSingleLine: true,
+          },
         ],
-        "vue/comma-dangle": [
-          "error",
-          "always-multiline"
+        'vue/comma-dangle': [
+          'error',
+          'always-multiline',
         ],
-        "vue/comma-spacing": [
-          "error",
+        'vue/comma-spacing': [
+          'error',
           {
-            "before": false,
-            "after": true
-          }
+            before: false,
+            after: true,
+          },
         ],
-        "vue/comma-style": [
-          "error",
-          "last"
+        'vue/comma-style': [
+          'error',
+          'last',
         ],
-        "vue/dot-location": [
-          "error",
-          "property"
+        'vue/dot-location': [
+          'error',
+          'property',
         ],
-        "vue/dot-notation": [
-          "error",
+        'vue/dot-notation': [
+          'error',
           {
-            "allowKeywords": true
-          }
+            allowKeywords: true,
+          },
         ],
-        "vue/eqeqeq": [
-          "error",
-          "smart"
+        'vue/eqeqeq': [
+          'error',
+          'smart',
         ],
-        "vue/key-spacing": [
-          "error",
+        'vue/key-spacing': [
+          'error',
           {
-            "beforeColon": false,
-            "afterColon": true
-          }
+            beforeColon: false,
+            afterColon: true,
+          },
         ],
-        "vue/keyword-spacing": [
-          "error",
+        'vue/keyword-spacing': [
+          'error',
           {
-            "before": true,
-            "after": true
-          }
+            before: true,
+            after: true,
+          },
         ],
-        "vue/no-constant-condition": "warn",
-        "vue/no-empty-pattern": "error",
-        "vue/no-extra-parens": [
-          "error",
-          "functions"
+        'vue/no-constant-condition': 'warn',
+        'vue/no-empty-pattern': 'error',
+        'vue/no-extra-parens': [
+          'error',
+          'functions',
         ],
-        "vue/no-irregular-whitespace": "error",
-        "vue/no-loss-of-precision": "error",
-        "vue/no-restricted-syntax": [
-          "error",
-          "DebuggerStatement",
-          "LabeledStatement",
-          "WithStatement"
+        'vue/no-irregular-whitespace': 'error',
+        'vue/no-loss-of-precision': 'error',
+        'vue/no-restricted-syntax': [
+          'error',
+          'DebuggerStatement',
+          'LabeledStatement',
+          'WithStatement',
         ],
-        "vue/no-sparse-arrays": "error",
-        "vue/object-curly-newline": [
-          "error",
+        'vue/no-sparse-arrays': 'error',
+        'vue/object-curly-newline': [
+          'error',
           {
-            "multiline": true,
-            "consistent": true
-          }
+            multiline: true,
+            consistent: true,
+          },
         ],
-        "vue/object-curly-spacing": [
-          "error",
-          "always"
+        'vue/object-curly-spacing': [
+          'error',
+          'always',
         ],
-        "vue/object-property-newline": [
-          "error",
+        'vue/object-property-newline': [
+          'error',
           {
-            "allowMultiplePropertiesPerLine": true
-          }
+            allowMultiplePropertiesPerLine: true,
+          },
         ],
-        "vue/object-shorthand": [
-          "error",
-          "always",
+        'vue/object-shorthand': [
+          'error',
+          'always',
           {
-            "ignoreConstructors": false,
-            "avoidQuotes": true
-          }
+            ignoreConstructors: false,
+            avoidQuotes: true,
+          },
         ],
-        "vue/operator-linebreak": [
-          "error",
-          "before"
+        'vue/operator-linebreak': [
+          'error',
+          'before',
         ],
-        "vue/prefer-template": "error",
-        "vue/quote-props": [
-          "error",
-          "consistent-as-needed"
+        'vue/prefer-template': 'error',
+        'vue/quote-props': [
+          'error',
+          'consistent-as-needed',
         ],
-        "vue/space-in-parens": [
-          "error",
-          "never"
+        'vue/space-in-parens': [
+          'error',
+          'never',
         ],
-        "vue/space-infix-ops": "error",
-        "vue/space-unary-ops": [
-          "error",
+        'vue/space-infix-ops': 'error',
+        'vue/space-unary-ops': [
+          'error',
           {
-            "words": true,
-            "nonwords": false
-          }
+            words: true,
+            nonwords: false,
+          },
         ],
-        "vue/template-curly-spacing": "error"
-      }
-    }
+        'vue/template-curly-spacing': 'error',
+      },
+    },
   ),
 )
